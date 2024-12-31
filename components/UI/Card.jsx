4595e8beca81f5e16f158f83a6834fd0e34a06f8
hadienzz <122720958+hadienzz@users.noreply.github.com>
2024-12-31 14:07:49 +0700

    feat:

--- components/UI/Card.jsx
+++ components/UI/Card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
         <div className="flex flex-col justify-center items-center text-[white] text-center">
             <div className="h-[300px] w-[300px] relative overflow-hidden">
-                <Image src={props.image} fill className="object-contain" alt={title} />
+                <Image src={props.image} fill className="object-contain" alt={props.title} />
             </div>
             <h1 className="text-[32px] leading-normal py-10 md:text-5xl">{props.title}</h1>
             <p className="max-w-[299px] md:max-w-[500px] md:text-2xl">{props.description}</p>

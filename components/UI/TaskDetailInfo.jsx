ad0f742aa38d3ab07f6eacca7de38f44e8728cb9
hadienzz <hadinpramiadi@gmail.com>
2025-02-19 21:29:26 +0700

    *

--- components/UI/TaskDetailInfo.jsx
+++ components/UI/TaskDetailInfo.jsx
@@ -1,17 +1,29 @@
-const TaskDetailInfo = ({ icon, title, description, categoryIcon }) => {
-    return (
-        <>
-            <div className="flex gap-2 text-primaryWhite">
-                <img src={icon.src} className="w-6 h-6" alt="" />
-                <p>{title}</p>
-            </div>
-            <div className="inline-flex justify-center items-center px-4 py-2 bg-[rgba(255,255,255,0.21)] text-primaryWhite rounded-md gap-[10px]">
-                {categoryIcon && <img src={categoryIcon} className="w-6 h-6 " alt="" />}
-                <p>{description}</p>
-            </div>
-        </>
+const TaskDetailInfo = ({
+  icon,
+  title,
+  description,
+  categoryIcon,
+  deleteTask,
+  ...props
+}) => {
+  return (
+    <>
+      <div className="flex justify-between" {...props}>
+        <div className="flex gap-2 text-primaryWhite">
+          <img src={icon.src} className="w-6 h-6" alt="" />
+          <p>{title}</p>
+        </div>
+        {!deleteTask && (
+          <div className="inline-flex justify-center items-center px-4 py-2 bg-[rgba(255,255,255,0.21)] text-primaryWhite rounded-md gap-[10px]">
+            {categoryIcon && (
+              <img src={categoryIcon} className="w-6 h-6 " alt="" />
+            )}
+            <p>{description}</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default TaskDetailInfo
+export default TaskDetailInfo;
--- src/app/context/TaskContextProvider.jsx
+++ src/app/context/TaskContextProvider.jsx
@@ -1,122 +1,132 @@
+import { useRouter } from "next/navigation";
+
 const { createContext, useState } = require("react");
 
-
 export const taskContext = createContext({
-    tasks: {},
-    openedModal: false,
-    priority: null,
-    category: null,
-    handleAddCategory: () => { },
-    handleTask: () => { },
-    handleAddTask: () => { },
-    modalHandler: () => { },
-    handleCloseModal: () => { },
-    handleAddPriority: () => { },
-    handleSelectCategory: () => { },
-})
+  tasks: {},
+  openedModal: false,
+  priority: null,
+  category: null,
+  handleAddCategory: () => {},
+  handleTask: () => {},
+  handleAddTask: () => {},
+  modalHandler: () => {},
+  handleCloseModal: () => {},
+  handleAddPriority: () => {},
+  handleSelectCategory: () => {},
+  handleDeleteTask: () => {},
+});
 
 const TaskContextProvider = ({ children }) => {
-    const [openedModal, setOpenedModal] = useState(false)
-    const [priority, setPriority] = useState(null)
-    const [category, setCategory] = useState(null)
-    const [error, setError] = useState(null)
-    const [currentTask, setCurrentTask] = useState({})
-    const [tasks, setTasks] = useState({
-        task: []
-    })
+  const [openedModal, setOpenedModal] = useState(false);
+  const [priority, setPriority] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [error, setError] = useState(null);
+  const [currentTask, setCurrentTask] = useState({});
+  const [tasks, setTasks] = useState({
+    task: [],
+  });
 
-    const handleTask = (event) => {
-        event.preventDefault()
-        const formData = new FormData(event.target)
+  const handleTask = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-        const enteredTitle = formData.get('title')
-        const enteredDescription = formData.get('description')
+    const enteredTitle = formData.get("title");
+    const enteredDescription = formData.get("description");
 
-        if (!enteredTitle || !enteredDescription) {
-            setError('Failed to add data due incomplete data')
-            return
-        }
-
-        const newTask = {
-            id: crypto.randomUUID(),
-            title: enteredTitle,
-            description: enteredDescription,
-        }
-
-        setCurrentTask(newTask)
-
-        modalHandler('category')
+    if (!enteredTitle || !enteredDescription) {
+      setError("Failed to add data due incomplete data");
+      return;
     }
 
-    const handleSelectCategory = (category) => {
-        setCategory(category)
-    }
-
-    const modalHandler = (type) => {
-        setOpenedModal(type)
-    }
-
-    const handleCloseModal = () => {
-        setOpenedModal(false)
-        setPriority(null)
-    }
-
-    const handleAddPriority = (num) => {
-        setPriority(num)
-        console.log(num)
-    }
-
-    const handleAddCategory = () => {
-        const newTask = {
-            ...currentTask,
-            category: category
-        }
-
-        if (!newTask.category) {
-            throw new Error('Please add priority first')
-        }
-
-        setCurrentTask(newTask)
-        modalHandler('priority')
+    const newTask = {
+      id: crypto.randomUUID(),
+      title: enteredTitle,
+      description: enteredDescription,
     };
 
-    const handleAddTask = () => {
-        const newTask = {
-            ...currentTask,
-            priority: priority
-        }
+    setCurrentTask(newTask);
 
-        if (!newTask.priority) {
-            throw new Error('Please add your priority first')
-        }
+    modalHandler("category");
+  };
 
-        setTasks((prevState) => ({
-            ...prevState,
-            task: [...prevState.task, newTask]
-        }))
-        handleCloseModal()
+  const handleSelectCategory = (category) => {
+    setCategory(category);
+  };
+
+  const modalHandler = (type) => {
+    setOpenedModal(type);
+  };
+
+  const handleCloseModal = () => {
+    setOpenedModal(false);
+    setPriority(null);
+  };
+
+  const handleAddPriority = (num) => {
+    setPriority(num);
+    console.log(num);
+  };
+
+  const handleAddCategory = () => {
+    const newTask = {
+      ...currentTask,
+      category: category,
+    };
+
+    if (!newTask.category) {
+      throw new Error("Please add priority first");
     }
 
-    const contextValue = {
-        openedModal,
-        priority,
-        error,
-        tasks,
-        handleTask,
-        modalHandler,
-        handleCloseModal,
-        handleAddPriority,
-        handleAddCategory,
-        category,
-        handleSelectCategory,
-        handleAddTask,
+    setCurrentTask(newTask);
+    modalHandler("priority");
+  };
+
+  const handleAddTask = () => {
+    const newTask = {
+      ...currentTask,
+      priority: priority,
+    };
+
+    if (!newTask.priority) {
+      throw new Error("Please add your priority first");
     }
 
+    setTasks((prevState) => ({
+      ...prevState,
+      task: [...prevState.task, newTask],
+    }));
+    handleCloseModal();
+  };
 
+  const handleDeleteTask = (id) => {
+    setTasks((prevState) => {
+      return {
+        ...prevState,
+        task: prevState.task.filter((item) => item.id !== id),
+      };
+    });
+  };
 
-    return (
-        <taskContext.Provider value={contextValue}>{children}</taskContext.Provider>
-    )
-}
+  const contextValue = {
+    openedModal,
+    priority,
+    error,
+    tasks,
+    handleTask,
+    modalHandler,
+    handleCloseModal,
+    handleAddPriority,
+    handleAddCategory,
+    category,
+    handleSelectCategory,
+    handleAddTask,
+    handleDeleteTask,
+  };
 
-export default TaskContextProvider
+  return (
+    <taskContext.Provider value={contextValue}>{children}</taskContext.Provider>
+  );
+};
+
+export default TaskContextProvider;

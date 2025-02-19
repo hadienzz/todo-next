2f061eac512465776916ecf722a95d4af6be29ec
hadienzz <hadinpramiadi@gmail.com>
2025-02-19 19:20:48 +0700

    feat(update): *

---
+++ components/UI/TaskDetailInfo.jsx
@@ -0,0 +1,17 @@
+const TaskDetailInfo = ({ icon, title, description, categoryIcon }) => {
+    return (
+        <>
+            <div className="flex gap-2 text-primaryWhite">
+                <img src={icon.src} className="w-6 h-6" alt="" />
+                <p>{title}</p>
+            </div>
+            <div className="inline-flex justify-center items-center px-4 py-2 bg-[rgba(255,255,255,0.21)] text-primaryWhite rounded-md gap-[10px]">
+                {categoryIcon && <img src={categoryIcon} className="w-6 h-6 " alt="" />}
+                <p>{description}</p>
+            </div>
+        </>
+
+    )
+}
+
+export default TaskDetailInfo
